93bce49e908c1d37251fd90c0af29acc00893f15
Kevin Lewis <get@jellybabi.es>
2015-10-29 15:27:13 +0000

    deploy from ship

--- js/main.js
+++ js/main.js
@@ -11,3 +11,24 @@
 	var filmImageHeight = $("#films img").height();
 
 	$("#films .meta").height(filmImageHeight);
+
+// Case Study
+
+	$("#case .image").height(
+		$(this).siblings(".content").height()
+	);
+
+	$("#case section > *").matchHeight();
+
+// jQuery Responsive
+
+	$(document).ready(function() {
+		checkSize();
+    	$(window).resize(checkSize);
+	});
+	
+	function checkSize(){
+	    if($("header").css("background") == "white"){
+	    	console.log("don't trigger me bro");
+	    }
+	}
